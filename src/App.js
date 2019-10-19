31570481ae17060a5f9e6a1836d9f1730d52f34f
Lucas Tiago <eulukasthyago@outlook.com>
2019-10-19 01:51:49 +0000

    Add Router and Topbar Navegation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
 import Login from './pages/Login';
 
 function App() {
 
     return (
         <Router>
+            <Header />
             <Switch>
+                <Router exact path="/">
+
+                </Router>
                 <Route path="/login">
                     <Login />
                 </Route>
+                <Router path="/register">
+
+                </Router>
             </Switch>
         </Router>
     );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { PageHeader, Button } from 'antd';
+import { Link } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import './style.css';
 
 
 export default function Header()
 {
+    let typeButton = (window.screen.width <= 576) ? "link" : "primary";
+
     return (
-        <>
+        <div className="headerPage">
             <PageHeader
-                onBack={()=>null}
+                onBack={()=>window.history.back()}
                 title="Messenger Blue"
+                ghost={false}
                 extra={[
-                    <Button type="dashed">Chat</Button>,
-                    <Button type="primary" >Chanel</Button>
+                    <Link key="1" to="/chat"><Button key="1" type={typeButton}>Chat</Button></Link>,
+                    <Link key="2" to="/chanel"><Button key="2" type={typeButton}>Chanel</Button></Link>,
+                    <Link key="3" to="/login"><Button type={typeButton}>Login</Button></Link>
                 ]}
             />
-        </>
+        </div>
     )
 }
